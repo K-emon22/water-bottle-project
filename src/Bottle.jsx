c6722f6280e009624c873c659201e58d14eba3e0
K-emon22 <emonsheikhkhalid2@gmail.com>
2025-04-04 11:52:50 +0600

    bottles

--- src/Bottle.jsx
+++ src/Bottle.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-const Bottle = ({bottl}) => {
-  console.log(bottl);
+const Bottle = ({bottl,handleAddToCart}) => {
+
+
 
   return (
-    <div className="border-4 gap-5 ">
-      <h1>{bottl.brand}</h1>
+    <div className="border-4 gap-5  ">
+      <h1 className="font-bold text-xl pl-2">Brand: {bottl.brand}</h1>
       <img
-        className="mx-auto my-auto w-full aspect-[3/4] object-cover"
+        className="mx-auto my-auto w-full aspect-[1/1] object-cover"
         src={bottl.image}
         alt={bottl.brand}
       />
+      <div className="text-center">
+      <button onClick={()=> handleAddToCart(bottl)} className="btn btn-primary "> buy now</button>
+      </div>
     </div>
   );
 };
--- src/components/Bottles.jsx
+++ src/components/Bottles.jsx
@@ -1,17 +1,25 @@
-import React, {use} from "react";
+import React, {use, useState} from "react";
 import Bottle from "../Bottle";
 const Bottles = ({PromiseBottle}) => {
+  const [cart, setCart] = useState([]);
+
   const bottles = use(PromiseBottle);
-  console.log(bottles);
+  const handleAddToCart = (bottl) => {
+    console.log("bottlea added", bottl);
+  };
 
   return (
     <div>
       <h1 className="font-bold text-5xl text-center">
         bottles: {bottles.length}
       </h1>
-      <div className="border-4 gap-5 border-red-600 grid grid-cols-3">
+      <div className="border-4 gap-5 border-red-600 grid grid-cols-4 p-5">
         {bottles.map((bottl) => (
-          <Bottle key={bottl.id} className="last:col-span-3" bottl={bottl}></Bottle>
+          <Bottle
+            key={bottl.id}
+            handleAddToCart={handleAddToCart}
+            bottl={bottl}
+          ></Bottle>
         ))}
       </div>
     </div>
